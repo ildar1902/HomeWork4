4fa6892711c7f2489df987984ad36ffa2a211f71
Home <ildar86@live.ru>
2022-11-07 18:11:00 +0500

    ДЗ 4, выполнено первое задание

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 20;
+        if (age >= 18)
+            System.out.println("Поздравляем Вас с совершеннолетием!");
+
+        else
+            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
+
+
+    }
+}
