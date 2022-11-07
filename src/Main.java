d4881661dba5a2e7898a60cc03f7699bde93648f
Home <ildar86@live.ru>
2022-11-07 23:11:34 +0500

    ДЗ 4, выполнено задание 3.1

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,71 @@
 public class Main {
     public static void main(String[] args) {
-//Задание 1
-       // int age = 20;
-     //   if (age >= 18) {
+//Задание 1.1
+        //  int age = 12;
+        //  if (age >= 18) {
         //    System.out.println("Поздравляем Вас с совершеннолетием!");
-     //   } else {
-      //      System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
+        // }
+        // if (age < 18) {
+        //    System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
 
-            // Задание 2
-     //   int age = 32;
-     //   if (age >= 7 && age < 18) {
-      //          System.out.println("Ребёнок ходит в Школу"); }
-      //  if (age >= 18 && age < 24) {
-      //          System.out.println("Человек окончил школу и может отправляться в университет"); }
-      //  if (age >= 24) {
-      //      System.out.println("Человек окончил университет и пора искать первую работу"); }
+        // Задание 1.2
+        //   int age = 32;
+        //   if (age >= 7 && age < 18) {
+        //          System.out.println("Ребёнок ходит в Школу"); }
+        //  if (age >= 18 && age < 24) {
+        //          System.out.println("Человек окончил школу и может отправляться в университет"); }
+        //  if (age >= 24) {
+        //      System.out.println("Человек окончил университет и пора искать первую работу"); }
 
-        // Задание 3
-        int NumberOfOccupiedPlaces = 103;
-        if (NumberOfOccupiedPlaces < 60) {
-            System.out.println("Есть свободные места");}
-        if (NumberOfOccupiedPlaces >= 60 && NumberOfOccupiedPlaces< 102) {
-                System.out.println("Остались только стоячие свободные места");}
-                if (NumberOfOccupiedPlaces >= 102) {
-                    System.out.println("Свободных мест нет"); }
+        // Задание 1.3
+        //   int NumberOfOccupiedPlaces = 103;
+        // if (NumberOfOccupiedPlaces < 60) {
+        //   System.out.println("Есть свободные места");}
+        // if (NumberOfOccupiedPlaces >= 60 && NumberOfOccupiedPlaces< 102) {
+        //       System.out.println("Остались только стоячие свободные места");}
+        //     if (NumberOfOccupiedPlaces >= 102) {
+        //       System.out.println("Свободных мест нет"); }
 
+        //Задание 2.1
+        //  int age = 11;
+        //   if (age >= 18) {
+        //  System.out.println("Поздравляем Вас с совершеннолетием!");
+        //  } else {
+        //   System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");}
 
+        //Задание 2.2
+        //  int age = 25;
+        //   if (age >= 7 && age < 18) {
+        //          System.out.println("Ребёнок ходит в Школу"); }
+        // if (age >= 18 &&age < 24) {
+        //       System.out.println("Человек окончил школу и может отправляться в университет"); }
+        //  else if (age >= 24) {
+        //    System.out.println("Человек окончил университет и пора искать первую работу"); }
+
+        // Задание 2.3
+        //   int NumberOfOccupiedPlaces = 73;
+        //    if (NumberOfOccupiedPlaces < 60) {
+        //      System.out.println("Есть свободные места");}
+        //    if (NumberOfOccupiedPlaces >= 60 && NumberOfOccupiedPlaces< 102) {
+        //          System.out.println("Остались только стоячие свободные места");} else if (NumberOfOccupiedPlaces >= 102) {
+        //          System.out.println("Свободных мест нет"); }
+
+        // Задание 3.1
+
+        int age = 28;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        if (age > 6 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу"); }
+        if (age > 18 && age < 24) {
+            System.out.println("Если возраст человека равен " + age + ", то его место в университете"); } else if (age >= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
         }
 
     }
 
-
+}
 
 
 
