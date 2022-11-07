0ca1549b6751bee63e227dfd8c373cd7a3dcb2a6
Home <ildar86@live.ru>
2022-11-07 19:17:01 +0500

    ДЗ 4, выполнено первое и второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+//Задание 1
+       // int age = 20;
+     //   if (age >= 18) {
+        //    System.out.println("Поздравляем Вас с совершеннолетием!");
+     //   } else {
+      //      System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
 
-        int age = 20;
-        if (age >= 18)
-            System.out.println("Поздравляем Вас с совершеннолетием!");
+            // Задание 2
+     //   int age = 32;
+     //   if (age >= 7 && age < 18) {
+      //          System.out.println("Ребёнок ходит в Школу"); }
+      //  if (age >= 18 && age < 24) {
+      //          System.out.println("Человек окончил школу и может отправляться в университет"); }
+      //  if (age >= 24) {
+      //      System.out.println("Человек окончил университет и пора искать первую работу"); }
 
-        else
-            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
 
+        }
 
     }
-}
+
+
+
+
+
